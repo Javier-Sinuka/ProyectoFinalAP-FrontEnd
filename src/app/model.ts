02ac1fbd0899cb4f919c9880bbf6c961b71c141b
JS <javier_sinuka@mi.unc.edu.ar>
2023-04-13 16:55:23 -0300

    Agregando

--- src/app/model.ts
+++ src/app/model.ts
@@ -18,15 +18,3 @@ export interface Laboral{
   periodoExperiencia: string;
   contenidoExperiencia: string;
 }
-
-export interface RespuestaAPI{
-  values: Value[];
-}
-export interface Value{
-  id: number;
-  lugarExperiencia: string;
-  modalidadExperiencia: string;
-  nombreExperiencia: string;
-  periodoExperiencia: string;
-  contenidoExperiencia: string;
-}
